ea6a19a9bd36af2a29e65cf84e69ebe8cd31081f
Shumon Chattopadhyay <s.chattopadhyay89@gmail.com>
2017-05-19 13:22:21 -0400

    Adds show info method to both Parent and Child classes

--- inheritance.py
+++ inheritance.py
@@ -4,15 +4,22 @@ class Parent():
         self.last_name = last_name
         self.eye_color = eye_color
 
+    def show_info(self):
+        print("Last Name - "+self.last_name)
+        print("Eye Color = "+self.eye_color)
+
 class Child(Parent):
     def __init__(self, last_name, eye_color, number_of_toys):
         print("Child Constructor Called")
         Parent.__init__(self, last_name, eye_color)
         self.number_of_toys = number_of_toys
 
+    def show_info(self):
+        Parent.show_info(self)
+        print("Number of toys - "+self.number_of_toys)
+
 king_triton = Parent("Triton", "Blue")
-print(king_triton.last_name)
+king_triton.show_info()
 
 ariel = Child("Triton", "Blue", "plenty!")
-print(ariel.last_name)
-print(ariel.number_of_toys)
+ariel.show_info()
